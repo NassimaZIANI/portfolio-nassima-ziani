a44c474b0c5175c48f5b94c07426c5ee3e38d660
NassimaZIANI <nassima.ziani1996@gmail.com>
2023-01-10 22:44:03 +0100

    added the needed libraries like bootstrap & firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'portfolio-86842',
+    appId: '1:710645014342:web:fcbd32e84b3d6a57860f47',
+    storageBucket: 'portfolio-86842.appspot.com',
+    apiKey: 'AIzaSyDTnJUUll8mWFcHFP50M0r70wEoCIyz8Fc',
+    authDomain: 'portfolio-86842.firebaseapp.com',
+    messagingSenderId: '710645014342',
+    measurementId: 'G-THL65LJWTZ',
+  },
   production: false
 };
 
