5e89939059c96755aaeae8a5004c94b87566c0b5
NassimaZIANI <nassima.ziani1996@gmail.com>
2023-01-13 18:47:14 +0100

    transparent navbar with home

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  // typescript
+  @ViewChild('navbar', { static: true }) navbar: ElementRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll', []) onWindowScroll() {
+    if (window.pageYOffset > 100) {
+      this.navbar.nativeElement.classList.add('navbar-scrolled');
+    } else {
+      this.navbar.nativeElement.classList.remove('navbar-scrolled');
+    }
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
